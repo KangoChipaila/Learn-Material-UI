ee79970aea43a45f8bb6c70d738989224001c9f8
KangoChipaila <kangomsindechipailam@gmail.com>
2023-11-07 08:19:39 +0200

    Button lessson done

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography 
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom>
+        Create a New Note
+      </Typography>
+
+      <Button 
+        onClick={() => console.log('You clicked me')}
+        type="submit" 
+        color="secondary"
+        variant='contained'>
+        Submit
+      </Button>
+    </Container>
   )
 }
