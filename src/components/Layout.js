7b6f045595011d1e4161c18193fa1a8f5e6bdc68
KangoChipaila <kangomsindechipailam@gmail.com>
2023-11-08 12:08:45 +0200

    Lesson 16 Complete

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,50 +1,117 @@
 import React from 'react'
-import { Drawer, Typography, makeStyles } from '@material-ui/core'
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import { AppBar, Toolbar, makeStyles } from '@material-ui/core'
+import Drawer from '@material-ui/core/Drawer'
+import Typography from '@material-ui/core/Typography'
+import { useHistory, useLocation } from 'react-router-dom'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons'
+import { format } from 'date-fns'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
-const useStyles = makeStyles({
-  page: {
-    background: '#f9f9f9',
-    width: '100%'
-  },
-  drawer: {
-    width: drawerWidth
-  },
-  drawerPaper: {
-    width: drawerWidth
-  },
-  root: {
-    display: 'flex'
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: '#f9f9f9',
+      width: '100%',
+      padding: theme.spacing(3),
+    },
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    active: {
+      background: '#f4f4f4'
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    appbar: {
+        width: `calc(100% - ${drawerWidth}px)`
+    },
+    toolbar: theme.mixins.toolbar
+    ,
+    date: {
+        flexGrow: 1
+    }
   }
 })
 
 export default function Layout({ children }) {
   const classes = useStyles()
+  const history = useHistory()
+  const location = useLocation()
+
+  const menuItems = [
+    { 
+      text: 'My Notes', 
+      icon: <SubjectOutlined color="secondary" />, 
+      path: '/' 
+    },
+    { 
+      text: 'Create Note', 
+      icon: <AddCircleOutlineOutlined color="secondary" />, 
+      path: '/create' 
+    },
+  ];
 
   return (
     <div className={classes.root}>
       {/* app bar */}
-      <div>app bar</div>
-      
-      {/* side drawer */}
+      <AppBar 
+        className={classes.appbar}
+        elevation={0}>
+        <Toolbar>
+            <Typography className={classes.date}>
+                Today is the { format(new Date(), 'do MMMM Y')}
+            </Typography>
+            <Typography>
+                Mario
+            </Typography>
+        </Toolbar>
+      </AppBar>
 
+      {/* side drawer */}
       <Drawer
         className={classes.drawer}
-        variant='permanent'
-        anchor='left'
-        classes={{ paper: classes.drawerPaper }}>
-        <Typography variant="h5">
-            <div>side drawer</div>
-        </Typography>
-      </Drawer>  
+        variant="permanent"
+        classes={{ paper: classes.drawerPaper }}
+        anchor="left"
+      >
+        <div>
+          <Typography variant="h5" className={classes.title}>
+            Ninja Notes
+          </Typography>
+        </div>
+
+        {/* links/list section */}
+        <List>
+          {menuItems.map((item) => (
+            <ListItem 
+              button 
+              key={item.text} 
+              onClick={() => history.push(item.path)}
+              className={location.pathname == item.path ? classes.active : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+        
+      </Drawer>
 
       {/* main content */}
       <div className={classes.page}>
+        <div className={classes.toolbar}></div>
         { children }
       </div>
     </div>
